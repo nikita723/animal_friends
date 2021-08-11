a644bd4b3ebee09e058ac2901bfc1dddbff26a7e
nikita723 <84617652+nikita723@users.noreply.github.com>
2021-08-11 15:11:56 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let upButton = document.querySelector('.pageup');
+console.log('pageup');
+window.onscroll = function () {
+    if (window.pageYOffset > 1500) {
+    upButton.classList.add('shown');
+  } else {
+    upButton.classList.remove('shown');
+  }
+}; 
+
+upButton.onclick = function () {
+   window.scrollTo(0, 0);
+};
+
+let nikita_yakovlev_anatolievich = 1;
+let bogdanov_viktor;
